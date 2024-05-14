d3a067100ead641c96e43ec2a2a7d3d19de21e82
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-14 16:17:46 -0400

    build fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: "development",
   output: {
     filename: "bundle.[fullhash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
