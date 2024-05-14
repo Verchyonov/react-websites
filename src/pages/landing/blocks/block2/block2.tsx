1c2652a58e8608ee29b0c5283c76e5992c01d2c1
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-14 19:26:55 -0400

    block 2 adjust

--- src/pages/landing/blocks/block2/block2.tsx
+++ src/pages/landing/blocks/block2/block2.tsx
@@ -22,15 +22,23 @@ export const Block2 = () => {
         viewport={{ once: true }}
       >
         <div className="w-full md:w-6/12 flex p-4 flex-col gap-4 text-white items-center justify-center">
-          <TextReg
-            customClass={"text-[#00BCF8] text-4xl mb-auto"}
-            text={"Tarot Of Meme"}
-          />
+          <div className="flex flex-col gap-2">
+            <TextReg
+              customClass={"text-[#00BCF8] text-4xl"}
+              text={"Tarot Of Meme"}
+            />
+            <TextReg
+              customClass={"text-[#00BCF8]"}
+              text={
+                "  is a deck of cards with meme pictures used for making crypto predictions."
+              }
+            />
+          </div>
           <div className="flex flex-col gap-8 my-auto">
             <TextReg
               customClass={"text-[#00BCF8]"}
               text={
-                "is a deck of cards with meme pictures used for making crypto predictions. Based on the real-life Tarot deck, it’s designed to be more fun and easy to use. The concept is similar to the Book of Meme or Museum of Meme, but unique. Our goal is to create a tool that uses your wallet address or coin CA to make spiritual predictions about other projects or personal funds."
+                " Based on the real-life Tarot deck, it’s designed to be more fun and easy to use. The concept is similar to the Book of Meme or Museum of Meme, but unique. Our goal is to create a tool that uses your wallet address or coin CA to make spiritual predictions about other projects or personal funds."
               }
             />
           </div>
