53526529b759640fe8de56efc10df6fe5d00d491
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-14 16:25:45 -0400

    fix images

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|webp)$/,
         exclude: /node_modules/,
-        type: "asset/resource",
+        type: "asset/inline",
       },
     ],
   },
