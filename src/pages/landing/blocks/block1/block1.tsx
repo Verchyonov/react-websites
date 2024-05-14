70edc99a88f997a19de93c3e979c71eb26d02a96
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-14 19:56:55 -0400

    rain

--- src/pages/landing/blocks/block1/block1.tsx
+++ src/pages/landing/blocks/block1/block1.tsx
@@ -7,12 +7,12 @@ import { Cards } from "../../../game/cards/cards2";
 
 export const Block1 = () => {
   return (
-    <div className="w-full min-h-[100vh] py-20 block1 justify-center items-center flex flex-col relative overflow-hidden">
-      {/* <img
-        className="absolute top-0 left-0 h-[100vh] z-20 w-48"
-        src="./block1/rain.gif"
-      />
+    <div className="w-full min-h-[100vh] py-20 block1 justify-center items-center flex flex-col relative overflow-hidden blurred-border-bottom">
       <img
+        className="absolute top-0 left-0 h-[100vh] z-20 w-full"
+        src="./rain.gif"
+      />
+      {/* <img
         className="absolute top-0 right-0 h-[100vh] z-20 w-48 img-hor"
         src="./block1/rain.gif"
       /> */}
