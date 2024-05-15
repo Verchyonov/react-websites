51268e89f6f5cab3319a648ebcde7b8a9fe220f7
Iurii Kondrakov <deezzir@gmail.com>
2024-05-15 18:05:03 -0400

    Add question icon

--- src/pages/game/cards/card.tsx
+++ src/pages/game/cards/card.tsx
@@ -1,12 +1,28 @@
 import React, { useRef } from "react";
 import { CSSTransition } from "react-transition-group";
+import LiveHelpIcon from "@mui/icons-material/LiveHelp";
 import "./card.css";
 
 export const Card = (props: any) => {
   const nodeRef = useRef(null);
+  const helpIconRef = useRef(null);
 
   return (
     <>
+      <CSSTransition
+        nodeRef={helpIconRef}
+        in={props.card.isFlipped}
+        timeout={300}
+        classNames="help-icon-anim"
+        unmountOnExit
+      >
+        <button
+          ref={helpIconRef}
+          className="absolute -top-[3.5rem] right-1/2 p-2 transform translate-x-1/2 hover:scale-110 transition-transform duration-300 ease-in-out"
+        >
+          <LiveHelpIcon style={{ color: "black", fontSize: "2.5rem" }} />
+        </button>
+      </CSSTransition>
       <CSSTransition
         nodeRef={nodeRef}
         in={props.card.isFlipped}
