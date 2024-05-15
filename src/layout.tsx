2c98c3d762aa4b5177abeac6ec86a55fcc527084
Iurii Kondrakov <deezzir@gmail.com>
2024-05-15 15:31:55 -0400

    Fixes

--- src/layout.tsx
+++ src/layout.tsx
@@ -10,7 +10,8 @@ export const Layout = (props: any) => {
       <Outlet />
       <img
         className="w-[45vh] md:w-[70vh] fixed bottom-0 md:bottom-10 left-1/2 md:left-10 z-50 md:translate-x-0 -translate-x-1/2"
-        src={"./notl.png"}
+        src={"/notl.png"}
+        alt="notl"
       />
     </>
   );
