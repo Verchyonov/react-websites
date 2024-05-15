e72958cb456d0c3f6fc196aabeca098c838af184
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-14 21:57:49 -0400

    c

--- src/pages/landing/blocks/block1/block1.tsx
+++ src/pages/landing/blocks/block1/block1.tsx
@@ -7,13 +7,13 @@ import { Cards } from "../../../game/cards/cards";
 
 export const Block1 = () => {
   return (
-    <div className="w-full min-h-[100vh] bg-opacity-10 py-20 block1 justify-center items-center flex flex-col relative overflow-hidden">
+    <div className="w-full min-h-[100vh] py-20 block1 justify-center items-center flex flex-col relative overflow-hidden">
       <img
-        className="absolute top-0 left-0 h-[100vh] z-20 w-1/2"
+        className="absolute top-0 left-0 h-[100vh] z-0 w-1/2"
         src="./rain.gif"
       />
       <img
-        className="absolute top-0 right-0 h-[100vh] z-20 w-1/2"
+        className="absolute top-0 right-0 h-[100vh] z-0 w-1/2"
         src="./rain.gif"
       />
       {/* <img
@@ -21,43 +21,47 @@ export const Block1 = () => {
         src="./block1/rain.gif"
       /> */}
       <img
-        className="absolute top-[-10%] lg:top-[-30%] w-[100vh] img-hor z-40"
+        className="absolute top-[-10%] lg:top-[-30%] w-[100vh] img-hor z-50 md:z-10"
         src="./block1/pepe.webp"
       />
       <img
-        src="./block1/oblako.webp"
-        className="absolute top-[-10vh] left-[-10vh] z-20 w-[50vh]"
+        src="./block1/oblako.png"
+        className="absolute top-[0] left-[-10vh] md:z-50  z-20 w-[40vh]"
       />
       <img
-        src="./block1/oblakol.webp"
-        className="absolute top-[-10vh] right-[-10vh] z-20 w-[50vh]"
+        src="./block1/oblakol.png"
+        className="absolute top-[0] right-[-10vh] md:z-50 z-20 w-[40vh]"
       />
-      <motion.div
+      <div
         className={
-          "w-11/12 max-w-screen-2xl bg-slate-300 flex flex-col p-4 py-12 gap-12 rounded-xl justify-center items-center z-50"
+          "w-11/12 max-w-screen-2xl block1inside flex flex-col p-4 py-12 gap-12 rounded-xl justify-center items-center z-50 md:z-20"
         }
-        initial={{
-          opacity: 0,
-        }}
-        whileInView={{
-          opacity: 1,
-        }}
-        transition={{
-          duration: 3,
-        }}
-        viewport={{ once: true }}
       >
-        <CopyCa />
-        <Links />
-        <TextReg
-          customClass={"mt-4 text-2xl text-center"}
-          text={"Lorem ipsun lorem ipsun lorem ipsun"}
-        />
+        <motion.div
+          className="w-full flex flex-col justify-center items-center gap-12"
+          initial={{
+            opacity: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 3,
+          }}
+          viewport={{ once: true }}
+        >
+          <CopyCa />
+          <Links />
+          <TextReg
+            customClass={"mt-4 text-2xl text-center"}
+            text={"Lorem ipsun lorem ipsun lorem ipsun"}
+          />
 
-        <div className="flex w-full flex-col gap-4 justify-center items-center">
-          <Cards />
-        </div>
-      </motion.div>
+          <div className="flex w-full flex-col gap-4 justify-center items-center">
+            <Cards />
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
