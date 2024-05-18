53602cff6511bca368e186cce770df59bf17500c
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-17 22:09:27 -0400

    1

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -31,11 +31,6 @@ export const Landing = () => {
           <FooterSection disclaimer={true} />
         </>
       )}
-      <img
-        className="w-[60vw] lg:w-[20vw] fixed bottom-5 lg:bottom-10 left-1/2 lg:left-10 z-50 lg:translate-x-0 -translate-x-1/2"
-        src={"/notl.png"}
-        alt="notl"
-      />
     </>
   );
 };
