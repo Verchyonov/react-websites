0f99a39004e74afe999f55511416e5a89e7eda93
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-17 20:21:44 -0400

    1

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -2,11 +2,16 @@ export const CA = "Coming";
 export const TELEGRAM = "http://t.me/TarotofMeme";
 export const TWITTER = "https://twitter.com/tarotofmeme";
 
-export const BUY_LINK = "";
-export const DEXSCREENER = "";
-export const RAYDIUM = "";
-export const JUPITER = "";
-export const DEXTOOLS = "";
+export const BUY_LINK =
+  "https://raydium.io/swap/?inputMint=sol&outputMint=4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R";
+export const DEXSCREENER =
+  "https://dexscreener.com/solana/FPrFB8hBiS6Mr5xy3yq4rZdDs8bWayBUa5hCRyqbSdnG";
+export const RAYDIUM =
+  "https://raydium.io/swap/?inputMint=sol&outputMint=4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R";
+export const JUPITER =
+  "https://jupiter-terminal.dexscreener.com/?inputMint=DNvXa6zFwwuKaqrpxFxtLB5cXmWNT88bH752JdDtFBEV&outputMint=So11111111111111111111111111111111111111112";
+export const DEXTOOLS =
+  "https://www.dextools.io/app/en/solana/pair-explorer/FPrFB8hBiS6Mr5xy3yq4rZdDs8bWayBUa5hCRyqbSdnG?t=1715991694994";
 
 export const TELEGRAM_DEV = "https://t.me/lettodev";
 export const TWITTER_DEV = "https://twitter.com/letto_dev";
